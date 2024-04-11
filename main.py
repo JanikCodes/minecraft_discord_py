f7720f17720e8c2cdf3e36920f62d7375d1ac121
JanikCodes <piefke2000@gmx.de>
2024-04-11 16:51:58 +0200

    feat: readded world count in bot status

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import os
 import logging
 import platform
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
+from sqlalchemy.orm import sessionmaker
+
+from Classes import World
 from executeQueue import ExecuteQueue
 
 # import required files
@@ -29,7 +32,17 @@ class Client(commands.Bot):
 
         logging.warning("Now logging..")
 
+        self.update_bot_status.start()
+
         ExecuteQueue(client=self).start()
+
+    @tasks.loop(seconds=30)
+    async def update_bot_status(self):
+        Session = sessionmaker(bind=database.engine)
+        session = Session()
+
+        world_count = session.query(World).count()
+        await self.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{world_count} worlds"))
 
 client = Client()
 client.run(os.getenv("token"))
